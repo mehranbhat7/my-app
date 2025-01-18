0e94aa221136e03002b5d40cfea14ed187769daf
Mehran <mehranbhat2001@gmail.com>
2025-01-18 15:57:09 +0530

    CRUD in nest completed

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll(@Query('role') role?: 'intern' | 'admin' | 'mod') {
+    return [];
+  }
+
+  @Get('interns')
+  findInterns() {
+    return [];
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatedUser: {}) {
+    return { id, ...updatedUser };
+  }
+  @Delete(':id')
+  deleteed(@Param('id') id: string) {
+    return { id };
+  }
+}
